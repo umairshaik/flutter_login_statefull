2ea6a0366b64ac626dee2d1cbcf7d53cb8d26310
Umair Shaik <umair.shaik@thoughtworks.com>
2023-12-19 20:08:54 +0530

    [Umair][001] add all files

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:login_stateful/src/screens/login_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Log me in",
+      home: Scaffold(body: LoginScreen()),
+    );
+  }
+}
