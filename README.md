8f9d3b83767a03a8d796ccdba036b3eea8d72fb3
umairshaik <u.shk.anjum@gmail.com>
2023-12-19 20:16:49 +0530

    [Umair][001] add all files (#1)
    Co-authored-by: Umair Shaik <umair.shaik@thoughtworks.com>

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# flutter_login_statefull
-This is a flutter project, this project demonstrate simple login page, and uses stateful widget concept.
+# login_stateful
+
+https://github.com/umairshaik/flutter_login_statefull.git
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:login_stateful/src/mixins/validation_mixin.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
+  final formKey = GlobalKey<FormState>();
+
+  String email = "";
+  String password = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.all(20.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              emailField(),
+              passwordField(),
+              submitButton(),
+            ],
+          ),
+        ));
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+        labelText: "Email Address",
+        hintText: "you@example.com",
+      ),
+      validator: validateEmail,
+      onSaved: (String? newValue) {
+        email = newValue!;
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: "Password",
+        hintText: "Password",
+      ),
+      validator: validationPwd,
+      onSaved: (String? newValue) {
+        password = newValue!;
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          var state = formKey.currentState;
+          if (state != null && state.validate()) {
+            state.save();
+            print("Time to post $email and $password to my API");
+          }
+        },
+        child: const Text("Submit"),
+      ),
+    );
+  }
+}
